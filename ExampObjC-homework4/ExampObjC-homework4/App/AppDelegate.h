ca77158122ed257a0be3b2f4720229d30d069a49
Alla <prirez@bk.ru>
2021-03-14 18:29:22 +0300

    homework4 started

---
+++ ExampObjC-homework4/ExampObjC-homework4/App/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  ExampObjC-homework4
+//
+//  Created by aprirez on 3/14/21.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
+
---
+++ ExampObjC-homework4/ExampObjC-homework4/Main/ViewController.m
@@ -0,0 +1,25 @@
+//
+//  ViewController.m
+//  ExampObjC-homework4
+//
+//  Created by aprirez on 3/14/21.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    
+    self.title = @"Map Example";
+    [self.view setBackgroundColor: [UIColor redColor]];
+    
+}
+
+
+@end
